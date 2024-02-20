d172bb48a5cac96b4964daf08f6caa0e6767eca3
Yazvillalba <yazminvillalba55@gmail.com>
2024-02-20 20:08:17 -0300

    error en backend

--- src/main/java/com/PortfolioVillalbaYazmin/YazminVillalba/Repository/IPersonaRepository.java
+++ src/main/java/com/PortfolioVillalbaYazmin/YazminVillalba/Repository/IPersonaRepository.java
@@ -5,6 +5,7 @@
 package com.PortfolioVillalbaYazmin.YazminVillalba.Repository;
 
 import com.PortfolioVillalbaYazmin.YazminVillalba.Entity.Persona;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +15,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface IPersonaRepository extends JpaRepository<Persona, Long>{
-    
+    public Optional<Persona> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
 }
